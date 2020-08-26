fa1fd2bb569b533eaf46df6ca8c05acab228a2e2
Aaron Brown <worldofaaron@hotmail.com>
2020-08-26 10:29:45 -0700

    Part5

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader #can be deleted later
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -20,16 +21,15 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         # return the last five published questions.
-        return Question.objects.order_by('-pub_date')[:5]
-
-
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/detail.html', {'question': question})
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        # Excludes any questions that aren't published yet.
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
